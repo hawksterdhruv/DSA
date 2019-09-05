ed21e7c452f5a04cbd4781acc49f8c79bd89099b
dhruv <hawksterdhruv@gmail.com>
2019-09-05 17:59:35 +0530

    v0.04 edit distance Dyn prog

--- lcs_dynamic_programming.py
+++ lcs_dynamic_programming.py
@@ -3,9 +3,9 @@ str2 = 'If last characters of both sequences do not match (or X[m-1] != Y[n-1]) 
 n = len(str1)
 m = len(str2)
 tabulation_matrix = [[0 for a in range(m+1)] for a in range(n+1)]
+
+# for debugging purposes
 # print(tabulation_matrix)
-i = n-1
-j = m-1
 
 for i in range(n+1):
     for j in range(m+1):
@@ -16,6 +16,8 @@ for i in range(n+1):
         elif str1[i-1] != str2[j-1]:
             tabulation_matrix[i][j] = max(tabulation_matrix[i][j-1],tabulation_matrix[i-1][j])
 
+# for debugging purposes
+# for a in tabulation_matrix:
+    # print(a)
 
-for a in tabulation_matrix:
-    print(a)
+print(tabulation_matrix[-1][-1])
